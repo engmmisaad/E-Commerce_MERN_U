a8641b53939deab227b98b7d20f3796fb6f4073b
Mohammd Saad <mmis1984@gmail.com>
2024-09-04 13:06:10 +0300

    Product features

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
 import userRoutes from "./routers/userRoutes";
+import productRouts from "./routers/productRoutes"
+import { seedInitalProducts } from "./services/productService";
 
 const app = express();
 const port = 3001;
 app.use(express.json());
 app.use("/user",userRoutes);
+app.use("/product",productRouts)
 mongoose
   .connect("mongodb://localhost:27017/ecommerce")
   .then(() => {
@@ -15,7 +18,7 @@ mongoose
     console.log("Can't connect mongooose DB");
   });
  
-
+  seedInitalProducts();
   app.listen(port,()=>{
     console.log(`server is up ! ...http://localhost:${port}`)
   });
